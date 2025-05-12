3ddefddb7b3e9d2ad020a62a45ef41a164d492ba
Karen Dantas <karennobre74@gmail.com>
2025-05-12 13:12:37 -0300

    feat: configure cors

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,9 @@ import (
 	"go-api/db"
 	"go-api/routes"
 
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -15,6 +18,15 @@ func main() {
 		fmt.Println("Error during the .env loading: ", err)
 	}
 	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	dbConnection, err := db.ConnectDB()
 	if err != nil {
